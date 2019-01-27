bafb512a3cbbaddb233ed3083ed607da6d2ad1fb
Adi Saputra <adisaputra.id@gmail.com>
2019-01-27 18:48:49 +0700

    update detection

--- detection.py
+++ detection.py
@@ -17,26 +17,4 @@ for eachObject in detections:
 
 image = Image.open('imagenew.jpg')
 image.show()
-    
-
-
-#  658  pip install imageai-2.0.2-py3-none-any.whl
-#  659  pip3 install imageai-2.0.2-py3-none-any.whl
-#  660  python3.6 -V
-#  661  python3.6 detection.py 
-#  662  nano detection.py
-#  663  python3.6 detection.py 
-#  664  nano detection.py
-#  665  python3.6 detection.py 
-#  666  pip3 install opencv-python
-#  667  python3.6 detection.py 
-#  668  pip3 install keras
-#  669  python3.6 detection.py 
-#  670  pip3 install tensorflow
-#  671  python3.6 detection.py 
-#  672  pip3 install cv2
-#  673  pip3 install images
-#  674  pip3 install Image
-#  675  python3.6 detection.py 
-#  676  pip3 install matplotlib
-#  677  python3.6 detection.py 
+  
